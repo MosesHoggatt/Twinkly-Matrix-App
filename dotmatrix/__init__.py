4349a45a9fc94ea6df4fd3ccb41e332ffa74b571
Moses Hoggatt <endlessninetyeight@gmail.com>
2026-01-04 16:09:20 -0700

    Add python project files

---
+++ dotmatrix/__init__.py
@@ -0,0 +1,5 @@
+from .dot_matrix import DotMatrix
+from .source_canvas import CanvasSource, SourcePreview
+from .video_renderer import VideoRenderer
+
+__all__ = ["DotMatrix", "CanvasSource", "SourcePreview", "VideoRenderer"]
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="TwinklyWall",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "pygame",
+    ],
+    author="Your Name",
+    description="A dot matrix display project",
+)
