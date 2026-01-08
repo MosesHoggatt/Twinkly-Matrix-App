fedf35c5f6e1401775698e1c902d39de9b01540d
Moses Hoggatt <endlessninetyeight@gmail.com>
2026-01-08 16:05:07 -0700

    First pass add input

--- TwinklyWall/games/tetris.py
+++ TwinklyWall/games/tetris.py
@@ -5,6 +5,7 @@ import os
 import pygame
 import numpy
 import random
+from logger import log
 from game_players import get_active_players_for_game, get_game_for_player
 from players import set_input_handler
 from pathlib import Path
@@ -17,11 +18,10 @@ class Tetris:
         self.blocks_width = 10
         self.blocks_height = 15 # Only 16.5 visible with current setup
         self.block_size = 3
-        print(f"block_size: {self.block_size}")
         self.screen = canvas
-        print(canvas)
         self.players = get_active_players_for_game
 
+        # There is probably a more concise way to do this:
         project_dir = Path(__file__).resolve().parent
         assets_file_path = (str)(project_dir / "assets")
         self.block_images = [pygame.image.load(assets_file_path + "/TetrisSquare_Empty.png"),
@@ -39,9 +39,41 @@ class Tetris:
         empty_block = "assets/tetris_blocks/TetrisSquare_Empty.png"
         self.grid = [[random.randrange(0, len(self.block_images)) for element in range(self.blocks_width)] for row in range(self.blocks_height)]
         print(self.screen.get_height())
-    # def bind_input():
-    #     for index, player in enumerate(players):
-    #         def on_input()
+   
+    def bind_input(self):
+        players = get_active_players_for_game("tetris")
+        for i, player in enumerate(players):
+            def make_input_handler(player_index=i):
+                def handle_tetris_input(self):
+                    cmd = payload.get("cmd")
+                    match cmd:
+                        case "MOVE_LEFT":
+                            move_piece_left()
+                        case "MOVE_RIGHT":
+                            move_piece_right()
+                        case "ROTATE_CLOCKWISE":
+                            rotate_clockwise()
+                        case "ROTATE_COUNTERCLOCKWISE":
+                            rotate_counterclockwise()
+                        case "HARD_DROP":
+                            hard_drop_piece()
+                return on_input
+            set_input_handler(player.player_id, make_input_handler())
+        
+       
+    def move_piece_left():
+        log("LEFT")
+    def move_piece_right():
+        log("RIGHT")
+
+    def rotate_clockwise():
+        log("ROTATE_CLOCKWISE")
+
+    def rotate_counterclockwise():
+        log("ROTATE__COUNTER_CLOCKWISE")
+
+    def hard_drop_piece():
+        log("HARD_DROP_PIECE")
 
     def tick(self): # Called in main
         
