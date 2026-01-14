8284d71e615957803fbebba57717216bc3e3d48c
Moses Hoggatt <endlessninetyeight@gmail.com>
2026-01-14 14:56:54 -0700

    Create symbolic link to give FPP permission to access file

--- TwinklyWall/games/tetris.py
+++ TwinklyWall/games/tetris.py
@@ -48,12 +48,12 @@ class Tetris:
         self.combo = 0
         self.points_reward = [0,1,3,5,8] # Index: num lines cleared at once
         self.was_last_score_tetris = False
-        script_path = Path(__file__).resolve().parent
-        img_path = str(script_path) + "/game_over_screen.png"
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        img_path = os.path.join(current_dir, 'game_over.png')
         print(img_path)
         # game_over_path = str(games_filepath) + "/game_over_screen.png"
         # game_over_img_path = sys.argv[0] + "/games/game_over_screen.png"
-        self.game_over_image = pygame.image.load(img_path).convert_alpha()
+        self.game_over_image = pygame.image.load(str(img_path)).convert_alpha()
         # self.game_over_image = pygame.image.load(abs_script_path).convert_alpha()
         # self.game_over_image = pygame.image.load("./game_over_screen.png").convert_alpha()
 
